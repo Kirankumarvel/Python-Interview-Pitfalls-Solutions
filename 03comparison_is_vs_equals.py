b6a65b9054496ff6ee6f0505f6642900e691a5d5
Kiran Kumar V <imkirankumar23@gmail.com>
2025-05-19 22:04:02 +0530

    Create 03comparison_is_vs_equals.py

---
+++ 03comparison_is_vs_equals.py
@@ -0,0 +1,7 @@
+# 3. Comparison Operators
+# comparison_is_vs_equals.py
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a == b)  # True (values are the same)
+print(a is b)  # False (different memory locations)
